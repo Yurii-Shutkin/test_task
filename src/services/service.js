2bce4ce79dc2a852a63bca994641ff2ac5240fb4
Yurii-Shutkin <yurashutkinn@gmail.com>
2025-03-19 09:17:01 +0200

    change API path

--- src/services/service.js
+++ src/services/service.js
@@ -1,4 +1,4 @@
-const API_URL = 'https://api.escuelajs.co/api/v1/products/1';
+const API_URL = 'https://api.escuelajs.co/api/v1/products/5';
 
 const get = async () => {
   const response = await fetch(API_URL);
